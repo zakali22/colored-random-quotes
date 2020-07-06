4e3f61dcfc4b14e5c1abe397d2ba6e484aa8b0ad
Zakeria Ali <zak.fined@gmail.com>
2020-07-06 18:58:38 +0100

    Remove unnecessary temp file

--- src/js/components.js
+++
@@ -1,7 +0,0 @@
-import $ from "jquery";
-
-// (function(){
-//     window.addEventListener("DOMContentLoaded", () => {
-//         $('.slide-footer').load("../components/footer.html")
-//     })
-// })()
